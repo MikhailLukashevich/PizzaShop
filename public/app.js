8d75bbcf18c1a6a9b2283e49792050eac4cb64e7
Mikhail <mishok_02@mail.ru>
2016-11-10 12:47:35 +0300

    Pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -6,3 +6,8 @@ function something()
     window.localStorage.setItem('bbb', x); // hh['bbb'] = x
     alert (x);
   }
+
+function add_to_cart(id)
+  {
+    alert("you added pizza with id:" + id);
+  }
