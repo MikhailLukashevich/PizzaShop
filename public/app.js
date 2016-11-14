eadefa8b35f4156c7b2ddf4265303b475db1bdc4
Mikhail <mishok_02@mail.ru>
2016-11-14 18:03:12 +0300

    Replace cart contents on cancel

--- public/app.js
+++ public/app.js
@@ -61,6 +61,9 @@ function cart_get_number_of_items()
     }
   function cancel_order()
     {
-      alert('aaa');
+      window.localStorage.clear();
+      update_orders_input();
+      update_orders_button();
+      $('#cart').text('Your cart is now empty');
       return false;
     }
