e66f5910ef0621df62844f3f958c85a1dd490184
Mikhail <mishok_02@mail.ru>
2016-11-14 15:19:32 +0300

    Add Orders model + migration

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ set :database, 'sqlite3:pizzashop.db'
 
 class Product < ActiveRecord::Base
 end
+class Order < ActiveRecord::Base
+end
 
 get '/' do
 	@products = Product.all
@@ -19,19 +21,21 @@ get '/about' do
 end
 
 post '/cart' do
-	orders_input = params[:orders]
-	@items = parse_orders_input orders_input
+	@orders_input = params[:orders]
+	@items = parse_orders_input @orders_input
 	@items.each do |item|
 		item[0] = Product.find(item[0])
 	end
+	#@o = {}
 	erb :cart
 end
 
 def parse_orders_input orders_input
-  s1 = orders_input.split(/,/)
+
+	s1 = orders_input.split(/,/)
   arr = []
   s1.each do |x|
-    s2 = x.split(/=/)
+    s2 = x.split(/\=/)
     s3 = s2[0].split(/_/)
     id = s3[1]
     cnt = s2[1]
